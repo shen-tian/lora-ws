7f8d75f5281a16d02ba3afa6a4977c7916a713ea
Shen Tian <biozinc@gmail.com>
2018-06-07 23:17:24 +0200

    Some cleanup. Autoformat by VSCode

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -19,13 +19,6 @@
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
-// Timing:
-#define TRANSMIT_INTERVAL 10000 // interval between sending updates
-#define DISPLAY_INTERVAL 150    // interval between updating display
-#define MAX_FIX_AGE 5000        // Ignore data from GPS if older
-
-// State var for radio/fix
-unsigned long lastSend, lastDisplay, lastFix;
 bool sending = false;
 
 #define CALLSIGN_LEN 4
@@ -39,7 +32,8 @@ uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
 
 float currentFreq = RF95_FREQ;
 
-void transmitData(float lat, float lon) {
+void transmitData(float lat, float lon)
+{
 
   char buff[20];
 
@@ -48,18 +42,20 @@ void transmitData(float lat, float lon) {
 
   uint8_t len = 2 * sizeof(int32_t) + sizeof(uint8_t) + CALLSIGN_LEN + MAGIC_NUMBER_LEN;
   uint8_t radiopacket[len];
-  for (int i = 0; i < MAGIC_NUMBER_LEN; i++) {
-     radiopacket[i] = MAGIC_NUMBER[i];
+  for (int i = 0; i < MAGIC_NUMBER_LEN; i++)
+  {
+    radiopacket[i] = MAGIC_NUMBER[i];
   }
-  for (uint8_t i = 0; i < CALLSIGN_LEN; i++) {
-     radiopacket[MAGIC_NUMBER_LEN + i] = '0';
+  for (uint8_t i = 0; i < CALLSIGN_LEN; i++)
+  {
+    radiopacket[MAGIC_NUMBER_LEN + i] = '0';
   }
-  void* p = radiopacket + MAGIC_NUMBER_LEN + CALLSIGN_LEN;
-  *(int32_t*)p = (int32_t)(lat * 1e6);
-  p = (int32_t*)p + 1;
-  *(int32_t*)p = (int32_t)(lon * 1e6);
-  p = (int32_t*)p + 1;
-  *(uint8_t*)p = true;
+  void *p = radiopacket + MAGIC_NUMBER_LEN + CALLSIGN_LEN;
+  *(int32_t *)p = (int32_t)(lat * 1e6);
+  p = (int32_t *)p + 1;
+  *(int32_t *)p = (int32_t)(lon * 1e6);
+  p = (int32_t *)p + 1;
+  *(uint8_t *)p = true;
 
   Serial.println("Saying Hai");
 
@@ -69,10 +65,10 @@ void transmitData(float lat, float lon) {
   rf95.waitPacketSent();
   digitalWrite(LED_BUILTIN, LOW);
   sending = false;
-  lastSend = millis();
 }
 
-void initRadio(){
+void initRadio()
+{
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
 
@@ -82,15 +78,18 @@ void initRadio(){
   digitalWrite(RFM95_RST, HIGH);
   delay(10);
 
-  while (!rf95.init()) {
+  while (!rf95.init())
+  {
     Serial.println("Radio init issue");
-    while (1);
+    while (1)
+      ;
   }
 
-
-  if (!rf95.setFrequency(RF95_FREQ)) {
+  if (!rf95.setFrequency(RF95_FREQ))
+  {
     Serial.println("Freq setting issue");
-    while (1);
+    while (1)
+      ;
   }
 
   //rf95.setModemConfig(RH_RF95::Bw125Cr45Sf128);
@@ -108,8 +107,8 @@ void initRadio(){
   Serial.println("Radio initiated!");
 }
 
-
-void setup() {
+void setup()
+{
 
   pinMode(LED_BUILTIN, OUTPUT);
 
@@ -121,33 +120,40 @@ void setup() {
 
 char readString[256];
 
-void loop() {
+void loop()
+{
 
-  if (Serial.available() > 0){
-     int len = Serial.readBytesUntil('\n', readString, 256);
-     
-     if (len > 0) {
-        readString[len] = 0;
-        Serial.println(len);
-        Serial.print("Read:");
-        Serial.print(readString);
-        Serial.println("!!");
+  if (Serial.available() > 0)
+  {
+    int len = Serial.readBytesUntil('\n', readString, 256);
 
-        DynamicJsonBuffer jsonBuffer; 
-        JsonObject& root = jsonBuffer.parseObject(readString);
+    if (len > 0)
+    {
+      readString[len] = 0;
+      Serial.println(len);
+      Serial.print("Read:");
+      Serial.print(readString);
+      Serial.println("!!");
 
-        float lat, lon;
+      DynamicJsonBuffer jsonBuffer;
+      JsonObject &root = jsonBuffer.parseObject(readString);
 
-        // Test if parsing succeeds.  
-        if (!root.success()) {
-          Serial.println("parseObject() failed");
-        } else {
-          Serial.println("success");
-          lat = root["lat"];
-          lon = root["lon"];
-        }
+      float lat = 0;
+      float lon = 0;
 
-        transmitData(lat, lon);
+      // Test if parsing succeeds.
+      if (!root.success())
+      {
+        Serial.println("parseObject() failed");
+      }
+      else
+      {
+        Serial.println("success");
+        lat = root["lat"];
+        lon = root["lon"];
+      }
+
+      transmitData(lat, lon);
     }
   }
 }
